b987d5c7f48a4761efb8029bf36eb312bb438139
Rupesh Bisen <rupesh.bisen2022@gmail.com>
2024-08-08 22:16:50 +0530

    navbar color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className='bg-gray-950 text-white fixed w-full z-20 top-0 left-0'>
+            <nav className='bg-gray-500 fixed w-full z-20 top-0 left-0'>
                 <div className='max-w-screen-2xl flex items-center justify-between mx-auto p-4 '>
                     <div onClick={() => router.push('/')} className='flex items-center cursor-pointer'>
                         <span className='text-xl md:text-3xl mx-1 md:mx-5'>🏠</span>
